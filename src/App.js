9dc02572a56c6f67e3c02ef7de7601b644741927
Cory Young <teigrgwyn@gmail.com>
2021-01-20 15:19:21 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"> 
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
